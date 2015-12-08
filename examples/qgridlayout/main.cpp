e29f2494a7643beb6a5e4f2477611d47ac34a663
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2015-12-08 16:31:57 +0100

    Add some stuff to qgridlayout example.

--- examples/qgridlayout/main.cpp
+++ examples/qgridlayout/main.cpp
@@ -1,3 +1,9 @@
+/*!
+ * References:
+ *
+ *   [1] https://youtu.be/G3iMyvzwQew
+ */
+
 #include <QApplication>
 #include <QtGui>
 #include <QTCore>
@@ -7,17 +13,18 @@ int main(int argc, char* argv[])
     QApplication app(argc, argv);
 
     QWidget* window = new QWidget;
+    window->setWindowTitle("My App");
 
     QGridLayout* layout = new QGridLayout;
 
-    QLabel* label1 = new QLabel("Name:");
-    QLineEdit* txtName = new QLineEdit;
+    QLabel* label1 = new QLabel("Name 1:");
+    QLineEdit* txtName1 = new QLineEdit;
 
-    QLabel* label2 = new QLabel("Name:");
+    QLabel* label2 = new QLabel("Name 2:");
     QLineEdit* txtName2 = new QLineEdit;
 
     layout->addWidget(label1, 0, 0);
-    layout->addWidget(txtName, 0, 1);
+    layout->addWidget(txtName1, 0, 1);
 
     layout->addWidget(label2, 1, 0);
     layout->addWidget(txtName2, 1, 1);
