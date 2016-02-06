39d83b4bcb21c36acc527ea55da814a19d368e82
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2016-02-07 00:45:12 +0100

    Add qstring example.

--- examples/qstring/qstring_example.cpp
+++ examples/qstring/qstring_example.cpp
@@ -17,4 +17,11 @@ int main()
   qDebug() << "theNullString.isNull() = " << theNullString.isNull();
   qDebug() << "theNullString.isEmpty() = " << theNullString.isEmpty();
 
+  // Don't do this ...
+  qDebug() << QString("%1 %2 %3").arg(1).arg(2).arg(3);
+  // ... but rather this (see http://doc.qt.io/qt-4.8/qstring.html#arg-2
+  // and https://youtu.be/Ov7s0GgBbOQ?t=45m40s)
+  qDebug() << QString("%1 %2 %3").arg(QString::number(1),
+                                      QString::number(2),
+                                      QString::number(3));
 }
