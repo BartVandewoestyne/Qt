db66658cf845f1823cccb75f73c085e72e9d84ca
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-05-31 12:13:32 +0200

    Add my own example on QWaitCondition.

---
+++ examples/qwaitcondition/waitconditions.cpp
@@ -0,0 +1,83 @@
+/*
+ * My own example about how to use QWaitCondition.
+ *
+ * Start by running this example, and observing how the output is not
+ * always 'Hello' followed by 'Bart' (sometimes it is, but this is by accident).
+ *
+ * Then, comment in the QWaitCondition code, and see how we now consequently
+ * have a 'Hello' followed by a 'Bart'.
+ *
+ * TODO: check for correctness!
+ */
+
+#include <QCoreApplication>
+#include <QDebug>
+#include <QMutex>
+#include <QObject>
+#include <QThread>
+#include <QWaitCondition>
+
+#include <cstdio>
+#include <cstdlib>
+
+QWaitCondition bartWritten;
+QWaitCondition helloWritten;
+QMutex mutex;
+
+class ThreadA : public QThread
+{
+    Q_OBJECT
+public:
+    ThreadA(QObject *parent = NULL) : QThread(parent)
+    {}
+
+    void run()
+    {
+        for (int i = 0; i < 10; ++i)
+        {
+            //if (i != 0)  // to avoid deadlock
+            //{
+            //    bartWritten.wait(&mutex);
+            //}
+            qDebug() << "Hello";
+            //helloWritten.wakeAll();
+        }
+    }
+};
+
+class ThreadB : public QThread
+{
+    Q_OBJECT
+public:
+    ThreadB(QObject *parent = NULL) : QThread(parent)
+    {}
+
+    void run()
+    {
+        for (int i = 0; i < 10; ++i)
+        {
+            //helloWritten.wait(&mutex);
+            qDebug() << "Bart";
+            //bartWritten.wakeAll();
+        }
+    }
+
+};
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+
+    ThreadA threadA;
+    ThreadB threadB;
+
+    threadA.start();
+    threadB.start();
+
+    threadA.wait();
+    threadB.wait();
+
+    return 0;
+}
+
+#include "waitconditions.moc"
