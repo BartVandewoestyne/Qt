3d8d3b94cb9140e2b1bb481757e84625d67cbe9c
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-01-18 22:45:34 +0100

    Improve QDomDocument example.

--- examples/qdomdocument/qdomdocument.cpp
+++ examples/qdomdocument/qdomdocument.cpp
@@ -31,32 +31,59 @@ int main(int argc, char *argv[])
         "    </two>"
         "</one>"
         ));
-
     assert(contentOK);
 
+    qDebug() << "Converting a QDomDocument to a QString:";
+    const int indent = -1;
+    qDebug() << "=> Method 1: using doc.toString(indent)";
+    qDebug() << doc.toString(indent);
+    qDebug() << "=> Method 2: using QTextStream";
+    QString str;
+    QTextStream stream(&str);
+    doc.save(stream, indent);
+    qDebug() << str;
+
+
+    qDebug() << "Finding child elements:";
     const auto firstChild = doc.firstChildElement();              // Returns <one> element and its recursive content.
-    const auto firstChildOne = doc.firstChildElement("one");      // Returns <one> element and its recursive content.
-    const auto firstChildTwo = doc.firstChildElement("two");      // DOES NOT WORK, returns ""
-    const auto firstChildThree = doc.firstChildElement("three");  // DOES NOT WORK, returns ""
-
     qDebug() << toString(firstChild);
-    qDebug() << toString(firstChildOne);
-    qDebug() << toString(firstChildTwo);
-    qDebug() << toString(firstChildThree);
 
-    const auto one = doc.firstChildElement("one");
-    const auto two = doc.firstChildElement("one").firstChildElement("two");
-    const auto three = doc.firstChildElement("one").firstChildElement("two").firstChildElement("three");
+    {
+        qDebug() << "=> Test 1:";
+        const auto one = doc.firstChildElement("one");      // Returns <one> element and its recursive content.
+        const auto two = doc.firstChildElement("two");      // DOES NOT WORK, returns ""
+        const auto three = doc.firstChildElement("three");  // DOES NOT WORK, returns ""
+        qDebug() << toString(one);
+        qDebug() << toString(two);
+        qDebug() << toString(three);
+    }
 
-    qDebug() << toString(one);
-    qDebug() << toString(two);
-    qDebug() << toString(three);
+    {
+        qDebug() << "=> Test 2:";
+        const auto one = doc.firstChildElement("one");
+        const auto two = doc.firstChildElement("one").firstChildElement("two");
+        const auto three = doc.firstChildElement("one").firstChildElement("two").firstChildElement("three");
+        qDebug() << toString(one);
+        qDebug() << toString(two);
+        qDebug() << toString(three);
+    }
 
-    const auto defaultConstructedDoc = QDomDocument();
-    qDebug() << defaultConstructedDoc.isNull();
-    
+    qDebug() << "Appending a child element to a QDomDocument that already has one:";
+    QDomDocument otherDoc;
+    const auto success = otherDoc.setContent(QString("<Other/>")); 
+    assert(success);
+
+    doc.appendChild(otherDoc.firstChildElement());
+    qDebug() << "After appending a child to a QDomDocument that already has a root element:";
+    qDebug() << doc.toString();
+
+    qDebug() << "After calling QDomDocument::clear(), doc.isNull() is:";
     doc.clear();
     qDebug() << doc.isNull();
+    
+    qDebug() << "Default constructed QDomDocument (isNull() test):";
+    const auto defaultConstructedDoc = QDomDocument();
+    qDebug() << defaultConstructedDoc.isNull();
 
     return a.exec();
 }
