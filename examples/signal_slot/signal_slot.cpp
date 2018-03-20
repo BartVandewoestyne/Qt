b6b5fc3727c51390b8e2ab4aedcfca6229d40c02
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-03-21 00:19:28 +0100

    Improve signal/slot references.

--- examples/signal_slot/signal_slot.cpp
+++ examples/signal_slot/signal_slot.cpp
@@ -1,5 +1,15 @@
 /*!
  * References:
- *   [1] https://www.kdab.com/disabling-narrowing-conversions-in-signal-slot-connections/
- *   [2] https://woboq.com/blog/how-qt-signals-slots-work.html
+ *
+ *   [dangelo20170412] Disabling narrowing conversions in signal/slot connections
+ *     https://www.kdab.com/disabling-narrowing-conversions-in-signal-slot-connections/
+ *
+ *   [goffart20160204] How Qt Signals and Slots Work - Part 3 - Queued and Inter Thread Connections
+ *     https://woboq.com/blog/how-qt-signals-slots-work-part3-queuedconnection.html
+ *
+ *   [goffart20121217] How Qt Signals and Slots Work - Part 2 - Qt5 New Syntax
+ *     https://woboq.com/blog/how-qt-signals-slots-work-part2-qt5.html
+ *
+ *   [goffart20121202] How Qt Signals and Slots Work
+ *     https://woboq.com/blog/how-qt-signals-slots-work.html
  */
