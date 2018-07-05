4330337934836f9af28fbbe9d77c1cb1a5529a1f
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-07-05 13:51:19 +0200

    Add references for Qt vs STL containers.

---
+++ qt_vs_stl_containers.cpp
@@ -0,0 +1,13 @@
+/*
+ * References:
+ *
+ *   [mutz20110815] Understand the Qt containers
+ *     https://marcmutz.wordpress.com/2011/08/15/effective-qt-understand-the-qt-containers/
+ *     https://marcmutz.wordpress.com/effective-qt/containers/
+ *
+ *   [julien20091103] STL or Qt containers?
+ *     https://stackoverflow.com/questions/1668259/stl-or-qt-containers
+ *
+ *   [blanchette2006] Inside the Qt 4 Containers
+ *     https://doc.qt.io/archives/qq/qq19-containers.html
+ */
