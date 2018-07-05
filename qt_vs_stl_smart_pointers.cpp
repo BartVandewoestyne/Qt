f30a4a3cc71cf6fd384b6984f012c856f5f826fe
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-07-05 13:57:52 +0200

    Add references for Qt vs STL smart pointers.

---
+++ qt_vs_stl_smart_pointers.cpp
@@ -0,0 +1,9 @@
+/*
+ * References:
+ *
+ *   [stackoverflow20110217] What C++ Smart Pointer Implementations are available?
+ *     https://stackoverflow.com/questions/5026197/what-c-smart-pointer-implementations-are-available/
+ *
+ *   [aliks??????] Qt smart pointers vs STL smart pointers
+ *     https://forum.qt.io/topic/76747/qt-smart-pointers-vs-stl-smart-pointers
+ */
