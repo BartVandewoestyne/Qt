16555a64b06dea80df320e00d35434997f5a1eee
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-07-13 14:23:18 +0200

    Add references for Qt's foreach vs C++11's range-based for.

--- foreach_vs_range_based_for.cpp
+++ foreach_vs_range_based_for.cpp
@@ -1,5 +1,22 @@
 /*
+ * The important remark seems to be that a range-based for loop has undefined
+ * behavior when adding and removing items from a container while iterating
+ * (see [logical20160829].  It is recommended to pass const containers to avoid
+ * the price of a copy.
+ *
  * References:
+ *
+ *   [evileg20180515] Qt/C++ - Tutorial 079. foreach vs range-based for in C++11?
+ *     https://evileg.com/en/post/348/
+ *
+ *   [stackoverflow20170515] How to iterate through a QStringList
+ *     https://stackoverflow.com/questions/44558568/how-to-iterate-through-a-qstringlist
+ *
+ *   [mutz20160829] Goodbye, Q_FOREACH
+ *     https://www.kdab.com/goodbye-q_foreach/
+ *
+ *   [logikal20160819] C++ 11 AND QT - PART 1
+ *     http://www.logikalsolutions.com/wordpress/information-technology/c-11-and-qt-part-1/
  *
  *   [stubert20160513] Simplifying Loops with C++11 in Qt Ways
  *     https://www.embeddeduse.com/2016/05/13/simplifying-loops-with-cpp11/
@@ -7,6 +24,6 @@
  *   [vratil20150524] QT CONTAINERS AND C++11 RANGE-BASED LOOPS
  *     https://www.dvratil.cz/2015/06/qt-containers-and-c11-range-based-loops/
  *
- *   [stackoverflow20170515] How to iterate through a QStringList
- *     https://stackoverflow.com/questions/44558568/how-to-iterate-through-a-qstringlist
+ *   [tranter20130904] Qt and C++11
+ *     https://www.ics.com/blog/qt-and-c11
  */
