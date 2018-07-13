cbcd642accb251fea112837c3888e6e11775f8bd
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-07-13 14:06:08 +0200

    Add references on Qt's for_each vs C++11's range-based for.

---
+++ foreach_vs_range_based_for.cpp
@@ -0,0 +1,12 @@
+/*
+ * References:
+ *
+ *   [stubert20160513] Simplifying Loops with C++11 in Qt Ways
+ *     https://www.embeddeduse.com/2016/05/13/simplifying-loops-with-cpp11/
+ *
+ *   [vratil20150524] QT CONTAINERS AND C++11 RANGE-BASED LOOPS
+ *     https://www.dvratil.cz/2015/06/qt-containers-and-c11-range-based-loops/
+ *
+ *   [stackoverflow20170515] How to iterate through a QStringList
+ *     https://stackoverflow.com/questions/44558568/how-to-iterate-through-a-qstringlist
+ */
