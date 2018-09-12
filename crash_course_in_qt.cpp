dc027e412e50d9b343ffbb882148ece2994d59bc
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-09-12 08:34:37 +0200

    Add references to crash course in Qt.

---
+++ crash_course_in_qt.cpp
@@ -0,0 +1,12 @@
+/*
+ * References:
+ *
+ *   [20180911fagrell] Signals and slots - communication between objects
+ *     https://www.cleanqt.io/blog/crash-course-in-qt-for-c%2B%2B-developers,-part-3
+ *
+ *   [20180904fagrell] Meta-object system (including QObject and MOC)
+ *     https://www.cleanqt.io/blog/crash-course-in-qt-for-c%2B%2B-developers,-part-2
+ *
+ *   [20180828fagrell] Events and the main event loop
+ *     https://www.cleanqt.io/blog/crash-course-in-qt-for-c%2B%2B-developers,-part-1
+ */
