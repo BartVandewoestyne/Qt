afdf5c4e491050bd152cbb168f3df4b4baf1609b
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-09-19 09:50:57 +0200

    Add reference for 'Hierarchie and memory management' blog post.

--- crash_course_in_qt.cpp
+++ crash_course_in_qt.cpp
@@ -1,12 +1,15 @@
 /*
  * References:
  *
- *   [20180911fagrell] Signals and slots - communication between objects
+ *   [fagrell20180918] Hierarchy and memory management
+ *     https://www.cleanqt.io/blog/crash-course-in-qt-for-c%2B%2B-developers,-part-4
+ *
+ *   [fagrell20180911] Signals and slots - communication between objects
  *     https://www.cleanqt.io/blog/crash-course-in-qt-for-c%2B%2B-developers,-part-3
  *
- *   [20180904fagrell] Meta-object system (including QObject and MOC)
+ *   [fagrell20180904] Meta-object system (including QObject and MOC)
  *     https://www.cleanqt.io/blog/crash-course-in-qt-for-c%2B%2B-developers,-part-2
  *
- *   [20180828fagrell] Events and the main event loop
+ *   [fagrell20180828] Events and the main event loop
  *     https://www.cleanqt.io/blog/crash-course-in-qt-for-c%2B%2B-developers,-part-1
  */
