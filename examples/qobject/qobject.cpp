f09feeda96aefd2611b09238ada4514e9ab4897b
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2020-01-22 18:13:47 +0100

    Add reference for QObject.

---
+++ examples/qobject/qobject.cpp
@@ -0,0 +1,7 @@
+/*
+ * References:
+ *
+ *   [stackoverflow] Defining own destructor although class derives from QObject?
+
+ *     https://stackoverflow.com/questions/34786769/defining-own-destructor-although-class-derives-from-qobject
+ */
