b196b7d026633f522fc603bcc20b81747167f841
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2020-10-08 14:27:01 +0200

    Add references for new signal/slot syntax.

--- examples/signal_slot/signal_slot.cpp
+++ examples/signal_slot/signal_slot.cpp
@@ -1,6 +1,15 @@
 /*!
  * References:
  *
+ *   [doc.qt.io] Differences between String-Based and Functor-Based Connections
+ *     https://doc.qt.io/qt-5/signalsandslots-syntaxes.html
+ *
+ *   [wiki.qt.io] New Signal Slot Syntax
+ *     https://wiki.qt.io/New_Signal_Slot_Syntax
+ *
+ *   [woboq.com] Signals and Slots in Qt5
+ *     https://woboq.com/blog/new-signals-slots-syntax-in-qt5.html
+ *
  *   [dangelo20170412] Disabling narrowing conversions in signal/slot connections
  *     https://www.kdab.com/disabling-narrowing-conversions-in-signal-slot-connections/
  *
