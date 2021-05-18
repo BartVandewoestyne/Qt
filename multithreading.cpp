25dbca4d52f91d290b67f6bdebef21185d1bdc7a
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-05-18 13:42:46 +0200

    Add references for multithreading.

---
+++ multithreading.cpp
@@ -0,0 +1,7 @@
+/*
+ * [smirnov] The Missing Article About Qt Multithreading in C++
+ *   https://www.toptal.com/qt/qt-multithreading-c-plus-plus
+ *
+ * [stackoverflow20161102] How to leverage Qt to make a QObject method thread-safe?
+ *   https://stackoverflow.com/questions/40382820/how-to-leverage-qt-to-make-a-qobject-method-thread-safe
+ */
