036b47b66c4701a43bba6664fe9ea4747d1a6ea9
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-11-14 23:42:46 +0100

    Add references for QStringView.

---
+++ Qt5/examples/qstringview.cpp
@@ -0,0 +1,9 @@
+/*
+ * References:
+ *
+ *   [mutz201710XX] QStringView QStringView everywhere - Marc Mutz
+ *     https://www.kdab.com/wp-content/uploads/stories/QStringViewEverywhere.pdf
+ *     
+ *   [mutz20170406] QStringView Diaries: The Eagle Has Landed
+ *     https://www.kdab.com/qstringview-diaries-eagle-landed/
+ */
---
+++ Qt6/examples/qstringview.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [cairns] Qt 6 - Episode 10 - QStringView
+ *     https://www.youtube.com/watch?v=ZwmpKaByCQ4
+ */
