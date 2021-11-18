d6ce8f75832186fb5ed1e8fb9ae73308b32fc3da
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-11-18 17:19:05 +0100

    Add reference for QAnyStringView.

---
+++ Qt6/examples/qanystringview.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [mutz20211112] Marc Mutz - QAnyStringView Variant String Views and Why You Care - Meeting C++ 2021 lightning talks
+ *     https://youtu.be/CcdK40EAaMA
+ */
